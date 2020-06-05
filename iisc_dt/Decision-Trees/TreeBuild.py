9c70c912b6a374412e7230a2b29f584c324b163d
kousikma <kousikimps@gmail.com>
2020-06-05 21:32:35 +0530

    iisc dt

--- iisc_dt/Decision-Trees/TreeBuild.py
+++
@@ -1,8 +0,0 @@
-import SplitData as sd
-import RecursiveSplitting as rs
-import pprint 
-
-def build_tree(train, max_depth, min_size):
-    root = sd.get_split(train)
-    rs.recursive_split(root, 1, max_depth, min_size)
-    return root
